ee8c6be96ce6f3a9dec3301e6178d8b2d5e25f88
Erick Lopez <71151050+LordLopez666@users.noreply.github.com>
2022-04-11 19:45:27 -0600

    changed time out for error page

--- src/Pages/ErrorPage.js
+++ src/Pages/ErrorPage.js
@@ -3,7 +3,7 @@ import { Navigate, useNavigate } from "react-router-dom";
 
 export default function ErrorPage() {
     const navigate = useNavigate();
-    useEffect(() => {setTimeout(() => {navigate("/")}, 2000)}, [navigate])
+    useEffect(() => {setTimeout(() => {navigate("/")}, 0.00001)}, [navigate])
     return (
         <div className="error--page">
             <h1>Whoops something went wrong!</h1>
